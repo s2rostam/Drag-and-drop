be1f2c657cacd786c38d8acf2f685bd662258987
s2rostam <33668049+s2rostam@users.noreply.github.com>
2021-03-30 13:27:55 -0400

    Add files via upload
    Created 10/19/2017

---
+++ Module-Drag and Drop/Module-Drag and Drop/Form1.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class DragAndDrop
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.StatusStrip1.SuspendLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 568)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(778, 22)
+        Me.StatusStrip1.TabIndex = 2
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(0, 17)
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox2.Image = Global.Module_Drag_and_Drop.My.Resources.Resources.Smiling_Face_Emoji_with_Blushed_Cheeks
+        Me.PictureBox2.InitialImage = Nothing
+        Me.PictureBox2.Location = New System.Drawing.Point(398, 266)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(243, 243)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Image = Global.Module_Drag_and_Drop.My.Resources.Resources.Smiling_Face_with_Tightly_Closed_eyes
+        Me.PictureBox1.InitialImage = Nothing
+        Me.PictureBox1.Location = New System.Drawing.Point(53, 77)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(243, 243)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'DragAndDrop
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(778, 590)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "DragAndDrop"
+        Me.Text = "Module Drag and Drop Sarah Rostami"
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
+End Class
---
+++ Module-Drag and Drop/Module-Drag and Drop/Form1.vb
@@ -0,0 +1,67 @@
+﻿Public Class DragAndDrop
+    Dim cur As Cursor = New Cursor(New IO.MemoryStream(My.Resources.H_MOVE))
+
+    Private Sub PictureBox1_MouseDown(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseDown
+        ChangeCursor()
+        PictureBox2.SendToBack()
+        ToolStripStatusLabel1.Text = PictureBox1.Location.ToString()
+    End Sub
+
+    Private Sub PictureBox2_MouseDown(sender As Object, e As MouseEventArgs) Handles PictureBox2.MouseDown
+        ChangeCursor()
+        PictureBox1.SendToBack()
+        ToolStripStatusLabel1.Text = PictureBox2.Location.ToString()
+    End Sub
+
+    Private Sub PictureBox1_MouseUp(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseUp
+        ResetDefaultCursor()
+    End Sub
+
+    Private Sub PictureBox2_MouseUp(sender As Object, e As MouseEventArgs) Handles PictureBox2.MouseUp
+        ResetDefaultCursor()
+    End Sub
+
+    Private Sub ChangeCursor()
+        Me.Cursor = cur
+    End Sub
+
+    Private Sub ResetDefaultCursor()
+        Me.Cursor = Cursors.Default
+    End Sub
+
+    Private Sub PictureBox1_MouseMove(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseMove
+        Static mousePosX As Single, mousePosY As Single
+        If (e.Button = 0) Then
+            mousePosX = e.X
+            mousePosY = e.Y
+        Else
+            PictureBox1.Left = PictureBox1.Left + (e.X - mousePosX)
+            PictureBox1.Top = PictureBox1.Top + (e.Y - mousePosY)
+            Dim intersect As Boolean = PictureBox1.Bounds.IntersectsWith(PictureBox2.Bounds)
+            If (intersect) Then
+                ToolStripStatusLabel1.Text = "PictureBox1: " + PictureBox1.Location.ToString + " || PictureBox1 is overlapping PictureBox2"
+            Else
+                ToolStripStatusLabel1.Text = "PictureBox1: " + PictureBox1.Location.ToString
+            End If
+        End If
+
+    End Sub
+
+    Private Sub PictureBox2_MouseMove(sender As Object, e As MouseEventArgs) Handles PictureBox2.MouseMove
+        Static mX As Single, mY As Single
+        If (e.Button = 0) Then
+            mX = e.X
+            mY = e.Y
+        Else
+            PictureBox2.Left = PictureBox2.Left + (e.X - mX)
+            PictureBox2.Top = PictureBox2.Top + (e.Y - mY)
+            Dim intersect As Boolean = PictureBox2.Bounds.IntersectsWith(PictureBox1.Bounds)
+            If (intersect) Then
+                ToolStripStatusLabel1.Text = "PictureBox2: " + PictureBox2.Location.ToString + " || PictureBox2 is overlapping PictureBox1"
+            Else
+                ToolStripStatusLabel1.Text = "PictureBox2: " + PictureBox2.Location.ToString
+            End If
+        End If
+    End Sub
+
+End Class
